bd5c10ab80651af04286a3dea360d27195274578
Anurag Dolui <144591392+1Anuraag0@users.noreply.github.com>
2025-03-24 13:27:36 +0530

    Changes to be committed:

---
+++ next.config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('next').NextConfig}
+ */
+const nextConfig = {
+  // ... existing config options ...
+  // swcMinify: true, // This line is causing the warning
+  // ... existing config options ...
+}
+
+module.exports = nextConfig 
